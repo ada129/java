f48f833edb561d15cb67c3a863fa3bd41a85c866
ada129 <91654820+ada129@users.noreply.github.com>
2022-10-25 22:35:26 +0300

    Add files via upload

---
+++ second/GameLogic.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GameLogic {
+       public static  void fight(Attacker attacker,Damageable target){
+                   attacker.attack(target);
+       };
+
+
+}
